f11d5438c8550bff4f2b833895187a6b586e12fc
Pierre BOQUET <jambonbill@users.noreply.github.com>
2018-03-14 00:21:14 +0800

    uo

--- NanoSyncroUSB.ino
+++ NanoSyncroUSB.ino
@@ -1,49 +1,36 @@
 // Jambon nano sync USBMIDI
-// Super simple LSDJ TO USBMIDI-SYNC (just what i need)
-// Use LSDJ Nano Sync
+// dead simple LSDJ TO USBMIDI-SYNC (just what i need)
+// Use LSDJ Nano Sync on Arduino Pro micro / Leonardo / etc
 // Send midi Start/Stop and Ticks
 // Plus a CC (31) on every beats
 
 #include "MIDIUSB.h"
 
-int beat_pin=8;
-int led_pin=9;
+int beat_pin=8;//blink
+//int led_pin=9;
 int analog_pin=18;//LSDJ NANO INPUT
 
 void setup(){   
-  pinMode(beat_pin, OUTPUT);//blink
-  pinMode(led_pin, OUTPUT);//blink  
-  
-  digitalWrite(beat_pin,1);
-  digitalWrite(led_pin,1);
-  delay(100);
-  digitalWrite(beat_pin,0);
-  digitalWrite(led_pin,0);
-  delay(100);
+  //hi
 }
 
-int val = 0; 
 bool play=0;
-bool last=0;//last state//
-bool tick=0;//clock up
+bool last=0;//last state
+bool tick=0;//clock state
 byte ticks=0;
-int steps=0;
-int beats=0;
+byte beats=0;
 int idle=0;
 
 void loop() {
 
-  val = analogRead(analog_pin);     // read the input pin
-  
+  int val = analogRead(analog_pin);
+  tick=0;
   if (val>10) {
-    //digitalWrite(led_pin,1);
     tick=1;
-  } else {
-    //digitalWrite(led_pin,0);
-    tick=0;
   }
   
   if(tick!=last){
+    
     ticks++;
     
     if(play==0){
@@ -56,17 +43,22 @@ void loop() {
     idle=0;
     
   }else{
-    //no change
-    idle++;
+    idle++;//no change
   }
 
   if(ticks>=24){// Regular 4/4
-  //if(ticks>=18){//CrazyQ ;]
+  //if(ticks>=36){//BIG SWING
     beatMidi();
     ticks=0;
-    steps++;
+    beats++;
+  }
+  
+  if(beats>=4){
+    barMidi();
+    beats=0;
   }
 
+  // blink to the beat
   if(play&&ticks<2){
     digitalWrite(beat_pin,1);
   }else{
@@ -80,18 +72,24 @@ void loop() {
   }
 }
 
-
 void start(){
   play=1;//playing
-  //Serial.println("START");
   startMidi();
 }
 
 void beatMidi(){
-  byte a=0xC0;
-  byte b=31;//CCNUM (arbitrary)
-  byte c=127;//MAX CCVAL 
-  // https://arduino.stackexchange.com/questions/41684/midiusb-why-is-the-command-put-twice
+  byte a=0xB0;// Control change
+  byte b=30;// CCNUM (i like this one)
+  byte c=127;// MAXIMUM POWER !!!  
+  midiEventPacket_t event = {0|a>>4, a, b, c};// https://arduino.stackexchange.com/questions/41684/midiusb-why-is-the-command-put-twice
+  MidiUSB.sendMIDI(event);
+  MidiUSB.flush();//send NOW
+}
+
+void barMidi(){//every 4 beats...
+  byte a=0xB0;// Control change
+  byte b=31;// CCNUM (i like this one)
+  byte c=127;// MAXIMUM POWER !!!  
   midiEventPacket_t event = {0|a>>4, a, b, c};
   MidiUSB.sendMIDI(event);
   MidiUSB.flush();//send NOW
@@ -111,7 +109,6 @@ void startMidi(){
   MidiUSB.flush();//send NOW
 }
 
-
 void stopMidi(){
   byte a=252;
   midiEventPacket_t event = {0|a>>4, a, 0, 0};
@@ -122,10 +119,8 @@ void stopMidi(){
 void stop(){
   if(play==0)return;//already stoped
   ticks=0;
-  steps=0;
   beats=0;
   idle=0;
-  play=0;
-  
+  play=0;  
   stopMidi();
 }
